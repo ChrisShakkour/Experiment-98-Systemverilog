6f801b7968d887024900f616b69e796c6b0507d3
ASIC Design3 <mlst3@vlsi-ria60.technion.ac.il>
2022-09-07 11:07:47 +0300

    adds lab 2 files after modifications

--- Top5.sv
+++ Top5.sv
@@ -13,20 +13,19 @@ module Top
     output logic [7:0] result
     );
 
-logic [`NoOfKernels-1:0] [7:0] convResult;
-logic [3:0] [7:0] pooledPixelArray[`NoOfKernels-1:0];
-logic [`numAddr-1:0] KMEM_ADD1;
-logic [`numAddr-1:0] KMEM_ADD2;
-logic [`numAddr-1:0] WMEM_ADD1;
-logic [`numAddr-1:0] WMEM_ADD2;
-logic KMEM_WEB1, KMEM_OEB1, KMEM_CSB1;
-logic KMEM_WEB2, KMEM_OEB2, KMEM_CSB2;
-logic WMEM_WEB1, WMEM_OEB1, WMEM_CSB1;
-logic WMEM_WEB2, WMEM_OEB2, WMEM_CSB2;
-logic [1:0][31:0] KR_DATA_O;
-logic [31:0] W1_DATA_O1, W1_DATA_O2,W2_DATA_O1, W2_DATA_O2;
-logic En;
-
+   logic [`NoOfKernels-1:0] [7:0] convResult;
+   logic [3:0] [7:0] 		  pooledPixelArray[`NoOfKernels-1:0];
+   logic [`numAddr-1:0] 	  KMEM_ADD1;
+   logic [`numAddr-1:0] 	  KMEM_ADD2;
+   logic [`numAddr-1:0] 	  WMEM_ADD1;
+   logic [`numAddr-1:0] 	  WMEM_ADD2;
+   logic 			  KMEM_WEB1, KMEM_OEB1, KMEM_CSB1;
+   logic 			  KMEM_WEB2, KMEM_OEB2, KMEM_CSB2;
+   logic 			  WMEM_WEB1, WMEM_OEB1, WMEM_CSB1;
+   logic 			  WMEM_WEB2, WMEM_OEB2, WMEM_CSB2;
+   logic [1:0][31:0] 		  KR_DATA_O;
+   logic [31:0] 		  W1_DATA_O1, W1_DATA_O2,W2_DATA_O1, W2_DATA_O2;
+   logic 			  En;
 
    NeuralNet_cont NNC_U1(.*);   
 
@@ -66,13 +65,33 @@ logic En;
       .O1	(W2_DATA_O1)	, .O2      (W2_DATA_O2)
       );
 
+   // NEURONS
+   generate 
+      for(genvar k=0; k<`NoOfKernels; k=k+1) begin
+	 CNeuron I_CNeuron
+		   (
+		    .clk	(clk), 
+		    .kernel	(KR_DATA_O[k]), 
+		    .pixels	(pixels), 
+		    .convResult	(convResult[k])
+		    );
+	 
+	 Pooling I_pooling
+	   (
+	    .clk		(clk), 
+	    .En			(En),
+	    .convResult		(convResult[k]),
+	    .pooledPixels	(pooledPixelArray[k])
+	    );
+      end
+   endgenerate   
 
-   
-// Example of generate statement	(required for experiment 2 later section)
-// genvar k;
-// generate for (k = 0; k <`NoOfKernels ; k = k + 1) begin
-// Code to be repeated for each k
-// end
-// endgenerate
+   FCNeuron I_FCNeuron
+     (
+      .clk		(clk), 
+      .pooledPixelArray	({pooledPixelArray[0],pooledPixelArray[1]}), 
+      .weight		({W1_DATA_O1, W2_DATA_O1}), 
+      .result		(result)
+      );
 
 endmodule
